ac2277e5053538296cf332821805e50beb74488b
gtrax147 <kobitlaainua>
2018-10-21 14:30:03 +0700

    Tung: Da code xong form MenuAnUong

--- QLKSdemo1/MenuAnUong.Designer.cs
+++ QLKSdemo1/MenuAnUong.Designer.cs
@@ -28,36 +28,325 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.AnPanel = new MetroFramework.Controls.MetroPanel();
+            this.AnhToi = new System.Windows.Forms.PictureBox();
+            this.AnhSang = new System.Windows.Forms.PictureBox();
+            this.AnhTrua = new System.Windows.Forms.PictureBox();
+            this.ToiCheckBox = new MetroFramework.Controls.MetroCheckBox();
+            this.SangCheckBox = new MetroFramework.Controls.MetroCheckBox();
+            this.TruaCheckBox = new MetroFramework.Controls.MetroCheckBox();
+            this.SangTextBox = new MetroFramework.Controls.MetroTextBox();
+            this.TruaTextBox = new MetroFramework.Controls.MetroTextBox();
+            this.ToiTextBox = new MetroFramework.Controls.MetroTextBox();
+            this.DVKhacPanel = new MetroFramework.Controls.MetroPanel();
+            this.MatXaCheckBox = new MetroFramework.Controls.MetroCheckBox();
+            this.HanhLyCheckBox = new MetroFramework.Controls.MetroCheckBox();
+            this.DonCheckBox = new MetroFramework.Controls.MetroCheckBox();
+            this.NutNext = new MetroFramework.Controls.MetroButton();
+            this.AnPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.AnhToi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AnhSang)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AnhTrua)).BeginInit();
+            this.DVKhacPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // metroPanel1
+            // AnPanel
             // 
-            this.metroPanel1.HorizontalScrollbarBarColor = true;
-            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
-            this.metroPanel1.HorizontalScrollbarSize = 10;
-            this.metroPanel1.Location = new System.Drawing.Point(23, 78);
-            this.metroPanel1.Name = "metroPanel1";
-            this.metroPanel1.Size = new System.Drawing.Size(488, 170);
-            this.metroPanel1.TabIndex = 0;
-            this.metroPanel1.VerticalScrollbarBarColor = true;
-            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
-            this.metroPanel1.VerticalScrollbarSize = 10;
+            this.AnPanel.Controls.Add(this.AnhToi);
+            this.AnPanel.Controls.Add(this.AnhSang);
+            this.AnPanel.Controls.Add(this.AnhTrua);
+            this.AnPanel.Controls.Add(this.ToiCheckBox);
+            this.AnPanel.Controls.Add(this.SangCheckBox);
+            this.AnPanel.Controls.Add(this.TruaCheckBox);
+            this.AnPanel.Controls.Add(this.SangTextBox);
+            this.AnPanel.Controls.Add(this.TruaTextBox);
+            this.AnPanel.Controls.Add(this.ToiTextBox);
+            this.AnPanel.HorizontalScrollbarBarColor = true;
+            this.AnPanel.HorizontalScrollbarHighlightOnWheel = false;
+            this.AnPanel.HorizontalScrollbarSize = 10;
+            this.AnPanel.Location = new System.Drawing.Point(23, 76);
+            this.AnPanel.Name = "AnPanel";
+            this.AnPanel.Size = new System.Drawing.Size(552, 182);
+            this.AnPanel.TabIndex = 0;
+            this.AnPanel.VerticalScrollbarBarColor = true;
+            this.AnPanel.VerticalScrollbarHighlightOnWheel = false;
+            this.AnPanel.VerticalScrollbarSize = 10;
+            // 
+            // AnhToi
+            // 
+            this.AnhToi.Location = new System.Drawing.Point(396, 15);
+            this.AnhToi.Name = "AnhToi";
+            this.AnhToi.Size = new System.Drawing.Size(144, 94);
+            this.AnhToi.TabIndex = 48;
+            this.AnhToi.TabStop = false;
+            // 
+            // AnhSang
+            // 
+            this.AnhSang.Location = new System.Drawing.Point(14, 15);
+            this.AnhSang.Name = "AnhSang";
+            this.AnhSang.Size = new System.Drawing.Size(144, 94);
+            this.AnhSang.TabIndex = 47;
+            this.AnhSang.TabStop = false;
+            // 
+            // AnhTrua
+            // 
+            this.AnhTrua.Location = new System.Drawing.Point(206, 15);
+            this.AnhTrua.Name = "AnhTrua";
+            this.AnhTrua.Size = new System.Drawing.Size(144, 94);
+            this.AnhTrua.TabIndex = 46;
+            this.AnhTrua.TabStop = false;
+            // 
+            // ToiCheckBox
+            // 
+            this.ToiCheckBox.AutoSize = true;
+            this.ToiCheckBox.BackColor = System.Drawing.Color.Transparent;
+            this.ToiCheckBox.FontSize = MetroFramework.MetroCheckBoxSize.Medium;
+            this.ToiCheckBox.Location = new System.Drawing.Point(396, 115);
+            this.ToiCheckBox.Name = "ToiCheckBox";
+            this.ToiCheckBox.Size = new System.Drawing.Size(113, 19);
+            this.ToiCheckBox.TabIndex = 45;
+            this.ToiCheckBox.Text = "Bữa Tối   (25k)";
+            this.ToiCheckBox.UseCustomBackColor = true;
+            this.ToiCheckBox.UseSelectable = true;
+            // 
+            // SangCheckBox
+            // 
+            this.SangCheckBox.AutoSize = true;
+            this.SangCheckBox.BackColor = System.Drawing.Color.Transparent;
+            this.SangCheckBox.FontSize = MetroFramework.MetroCheckBoxSize.Medium;
+            this.SangCheckBox.Location = new System.Drawing.Point(14, 115);
+            this.SangCheckBox.Name = "SangCheckBox";
+            this.SangCheckBox.Size = new System.Drawing.Size(117, 19);
+            this.SangCheckBox.TabIndex = 44;
+            this.SangCheckBox.Text = "Bữa Sáng (15k)";
+            this.SangCheckBox.UseCustomBackColor = true;
+            this.SangCheckBox.UseSelectable = true;
+            // 
+            // TruaCheckBox
+            // 
+            this.TruaCheckBox.AutoSize = true;
+            this.TruaCheckBox.BackColor = System.Drawing.Color.Transparent;
+            this.TruaCheckBox.FontSize = MetroFramework.MetroCheckBoxSize.Medium;
+            this.TruaCheckBox.Location = new System.Drawing.Point(206, 115);
+            this.TruaCheckBox.Name = "TruaCheckBox";
+            this.TruaCheckBox.Size = new System.Drawing.Size(118, 19);
+            this.TruaCheckBox.TabIndex = 43;
+            this.TruaCheckBox.Text = "Bữa Trưa  (20k)";
+            this.TruaCheckBox.UseCustomBackColor = true;
+            this.TruaCheckBox.UseSelectable = true;
+            // 
+            // SangTextBox
+            // 
+            this.SangTextBox.BackColor = System.Drawing.Color.White;
+            // 
+            // 
+            // 
+            this.SangTextBox.CustomButton.Image = null;
+            this.SangTextBox.CustomButton.Location = new System.Drawing.Point(116, 1);
+            this.SangTextBox.CustomButton.Name = "";
+            this.SangTextBox.CustomButton.Size = new System.Drawing.Size(27, 27);
+            this.SangTextBox.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.SangTextBox.CustomButton.TabIndex = 1;
+            this.SangTextBox.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.SangTextBox.CustomButton.UseSelectable = true;
+            this.SangTextBox.CustomButton.Visible = false;
+            this.SangTextBox.Enabled = false;
+            this.SangTextBox.FontSize = MetroFramework.MetroTextBoxSize.Medium;
+            this.SangTextBox.Lines = new string[0];
+            this.SangTextBox.Location = new System.Drawing.Point(14, 140);
+            this.SangTextBox.MaxLength = 32767;
+            this.SangTextBox.Name = "SangTextBox";
+            this.SangTextBox.PasswordChar = '\0';
+            this.SangTextBox.PromptText = "Số Lượng #";
+            this.SangTextBox.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.SangTextBox.SelectedText = "";
+            this.SangTextBox.SelectionLength = 0;
+            this.SangTextBox.SelectionStart = 0;
+            this.SangTextBox.ShortcutsEnabled = true;
+            this.SangTextBox.Size = new System.Drawing.Size(144, 29);
+            this.SangTextBox.Style = MetroFramework.MetroColorStyle.White;
+            this.SangTextBox.TabIndex = 42;
+            this.SangTextBox.UseCustomBackColor = true;
+            this.SangTextBox.UseSelectable = true;
+            this.SangTextBox.WaterMark = "Số Lượng #";
+            this.SangTextBox.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.SangTextBox.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // TruaTextBox
+            // 
+            this.TruaTextBox.BackColor = System.Drawing.Color.White;
+            // 
+            // 
+            // 
+            this.TruaTextBox.CustomButton.Image = null;
+            this.TruaTextBox.CustomButton.Location = new System.Drawing.Point(116, 1);
+            this.TruaTextBox.CustomButton.Name = "";
+            this.TruaTextBox.CustomButton.Size = new System.Drawing.Size(27, 27);
+            this.TruaTextBox.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.TruaTextBox.CustomButton.TabIndex = 1;
+            this.TruaTextBox.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.TruaTextBox.CustomButton.UseSelectable = true;
+            this.TruaTextBox.CustomButton.Visible = false;
+            this.TruaTextBox.Enabled = false;
+            this.TruaTextBox.FontSize = MetroFramework.MetroTextBoxSize.Medium;
+            this.TruaTextBox.Lines = new string[0];
+            this.TruaTextBox.Location = new System.Drawing.Point(206, 140);
+            this.TruaTextBox.MaxLength = 32767;
+            this.TruaTextBox.Name = "TruaTextBox";
+            this.TruaTextBox.PasswordChar = '\0';
+            this.TruaTextBox.PromptText = "Số Lượng #";
+            this.TruaTextBox.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.TruaTextBox.SelectedText = "";
+            this.TruaTextBox.SelectionLength = 0;
+            this.TruaTextBox.SelectionStart = 0;
+            this.TruaTextBox.ShortcutsEnabled = true;
+            this.TruaTextBox.Size = new System.Drawing.Size(144, 29);
+            this.TruaTextBox.Style = MetroFramework.MetroColorStyle.White;
+            this.TruaTextBox.TabIndex = 41;
+            this.TruaTextBox.UseCustomBackColor = true;
+            this.TruaTextBox.UseSelectable = true;
+            this.TruaTextBox.WaterMark = "Số Lượng #";
+            this.TruaTextBox.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.TruaTextBox.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // ToiTextBox
+            // 
+            this.ToiTextBox.BackColor = System.Drawing.Color.White;
+            // 
+            // 
+            // 
+            this.ToiTextBox.CustomButton.Image = null;
+            this.ToiTextBox.CustomButton.Location = new System.Drawing.Point(116, 1);
+            this.ToiTextBox.CustomButton.Name = "";
+            this.ToiTextBox.CustomButton.Size = new System.Drawing.Size(27, 27);
+            this.ToiTextBox.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.ToiTextBox.CustomButton.TabIndex = 1;
+            this.ToiTextBox.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.ToiTextBox.CustomButton.UseSelectable = true;
+            this.ToiTextBox.CustomButton.Visible = false;
+            this.ToiTextBox.Enabled = false;
+            this.ToiTextBox.FontSize = MetroFramework.MetroTextBoxSize.Medium;
+            this.ToiTextBox.Lines = new string[0];
+            this.ToiTextBox.Location = new System.Drawing.Point(396, 140);
+            this.ToiTextBox.MaxLength = 32767;
+            this.ToiTextBox.Name = "ToiTextBox";
+            this.ToiTextBox.PasswordChar = '\0';
+            this.ToiTextBox.PromptText = "Số Lượng #";
+            this.ToiTextBox.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.ToiTextBox.SelectedText = "";
+            this.ToiTextBox.SelectionLength = 0;
+            this.ToiTextBox.SelectionStart = 0;
+            this.ToiTextBox.ShortcutsEnabled = true;
+            this.ToiTextBox.Size = new System.Drawing.Size(144, 29);
+            this.ToiTextBox.Style = MetroFramework.MetroColorStyle.White;
+            this.ToiTextBox.TabIndex = 40;
+            this.ToiTextBox.UseCustomBackColor = true;
+            this.ToiTextBox.UseSelectable = true;
+            this.ToiTextBox.WaterMark = "Số Lượng #";
+            this.ToiTextBox.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.ToiTextBox.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // DVKhacPanel
+            // 
+            this.DVKhacPanel.Controls.Add(this.MatXaCheckBox);
+            this.DVKhacPanel.Controls.Add(this.HanhLyCheckBox);
+            this.DVKhacPanel.Controls.Add(this.DonCheckBox);
+            this.DVKhacPanel.HorizontalScrollbarBarColor = true;
+            this.DVKhacPanel.HorizontalScrollbarHighlightOnWheel = false;
+            this.DVKhacPanel.HorizontalScrollbarSize = 10;
+            this.DVKhacPanel.Location = new System.Drawing.Point(23, 287);
+            this.DVKhacPanel.Name = "DVKhacPanel";
+            this.DVKhacPanel.Size = new System.Drawing.Size(552, 74);
+            this.DVKhacPanel.TabIndex = 2;
+            this.DVKhacPanel.VerticalScrollbarBarColor = true;
+            this.DVKhacPanel.VerticalScrollbarHighlightOnWheel = false;
+            this.DVKhacPanel.VerticalScrollbarSize = 10;
+            // 
+            // MatXaCheckBox
+            // 
+            this.MatXaCheckBox.AutoSize = true;
+            this.MatXaCheckBox.BackColor = System.Drawing.Color.Transparent;
+            this.MatXaCheckBox.FontSize = MetroFramework.MetroCheckBoxSize.Medium;
+            this.MatXaCheckBox.Location = new System.Drawing.Point(396, 41);
+            this.MatXaCheckBox.Name = "MatXaCheckBox";
+            this.MatXaCheckBox.Size = new System.Drawing.Size(69, 19);
+            this.MatXaCheckBox.TabIndex = 48;
+            this.MatXaCheckBox.Text = "Mát-xa";
+            this.MatXaCheckBox.UseCustomBackColor = true;
+            this.MatXaCheckBox.UseSelectable = true;
+            // 
+            // HanhLyCheckBox
+            // 
+            this.HanhLyCheckBox.BackColor = System.Drawing.Color.Transparent;
+            this.HanhLyCheckBox.FontSize = MetroFramework.MetroCheckBoxSize.Medium;
+            this.HanhLyCheckBox.Location = new System.Drawing.Point(206, 41);
+            this.HanhLyCheckBox.Name = "HanhLyCheckBox";
+            this.HanhLyCheckBox.Size = new System.Drawing.Size(106, 19);
+            this.HanhLyCheckBox.TabIndex = 47;
+            this.HanhLyCheckBox.Text = "Phụ Hành Lý";
+            this.HanhLyCheckBox.UseCustomBackColor = true;
+            this.HanhLyCheckBox.UseSelectable = true;
+            // 
+            // DonCheckBox
+            // 
+            this.DonCheckBox.AutoSize = true;
+            this.DonCheckBox.BackColor = System.Drawing.Color.Transparent;
+            this.DonCheckBox.FontSize = MetroFramework.MetroCheckBoxSize.Medium;
+            this.DonCheckBox.Location = new System.Drawing.Point(14, 41);
+            this.DonCheckBox.Name = "DonCheckBox";
+            this.DonCheckBox.Size = new System.Drawing.Size(80, 19);
+            this.DonCheckBox.TabIndex = 46;
+            this.DonCheckBox.Text = "Dọn Dẹp";
+            this.DonCheckBox.UseCustomBackColor = true;
+            this.DonCheckBox.UseSelectable = true;
+            // 
+            // NutNext
+            // 
+            this.NutNext.Location = new System.Drawing.Point(411, 383);
+            this.NutNext.Name = "NutNext";
+            this.NutNext.Size = new System.Drawing.Size(164, 34);
+            this.NutNext.TabIndex = 49;
+            this.NutNext.Text = "Tiếp tục";
+            this.NutNext.UseSelectable = true;
+            this.NutNext.Click += new System.EventHandler(this.NutNext_Click);
             // 
             // MenuAnUong
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(534, 442);
-            this.Controls.Add(this.metroPanel1);
+            this.ClientSize = new System.Drawing.Size(596, 431);
+            this.Controls.Add(this.NutNext);
+            this.Controls.Add(this.DVKhacPanel);
+            this.Controls.Add(this.AnPanel);
             this.Name = "MenuAnUong";
             this.Text = "MenuAnUong";
+            this.AnPanel.ResumeLayout(false);
+            this.AnPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.AnhToi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AnhSang)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AnhTrua)).EndInit();
+            this.DVKhacPanel.ResumeLayout(false);
+            this.DVKhacPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private MetroFramework.Controls.MetroPanel AnPanel;
+        private System.Windows.Forms.PictureBox AnhToi;
+        private System.Windows.Forms.PictureBox AnhSang;
+        private System.Windows.Forms.PictureBox AnhTrua;
+        public MetroFramework.Controls.MetroCheckBox ToiCheckBox;
+        public MetroFramework.Controls.MetroCheckBox SangCheckBox;
+        public MetroFramework.Controls.MetroCheckBox TruaCheckBox;
+        public MetroFramework.Controls.MetroTextBox SangTextBox;
+        public MetroFramework.Controls.MetroTextBox TruaTextBox;
+        public MetroFramework.Controls.MetroTextBox ToiTextBox;
+        private MetroFramework.Controls.MetroPanel DVKhacPanel;
+        public MetroFramework.Controls.MetroCheckBox MatXaCheckBox;
+        public MetroFramework.Controls.MetroCheckBox HanhLyCheckBox;
+        public MetroFramework.Controls.MetroCheckBox DonCheckBox;
+        public MetroFramework.Controls.MetroButton NutNext;
     }
 }
--- QLKSdemo1/MenuAnUong.cs
+++ QLKSdemo1/MenuAnUong.cs
@@ -16,5 +16,106 @@ namespace QLKSdemo1
         {
             InitializeComponent();
         }
+
+        private int yeuCauSang = 0;
+
+        public int YeuCauSang
+        {
+            get { return yeuCauSang; }
+            set { yeuCauSang = value; }
+        }
+
+        private int yeuCauTrua = 0;
+
+        public int YeuCauTrua
+        {
+            get { return yeuCauTrua; }
+            set { yeuCauTrua = value; }
+        }
+
+        private int yeuCauToi = 0;
+
+        public int YeuCauToi
+        {
+            get { return yeuCauToi; }
+            set { yeuCauToi = value; }
+        }
+
+        private string yeuCauDon = "";
+
+        public string YeuCauDon
+        {
+            get { return yeuCauDon; }
+            set { yeuCauDon = value; }
+        }
+        private string yeuCauHanhLy = "";
+
+        public string YeuCauHanhLy
+        {
+            get { return yeuCauHanhLy; }
+            set { yeuCauHanhLy = value; }
+        }
+
+        private string yeuCauMatXa = "";
+
+        public string YeuCauMatXa
+        {
+            get { return yeuCauMatXa; }
+            set { yeuCauMatXa = value; }
+        }
+
+        private void NutNext_Click(object sender, EventArgs e)
+        {
+            if (SangCheckBox.Checked)
+            {
+                yeuCauSang = Convert.ToInt32(SangTextBox.Text);
+            }
+            if (TruaCheckBox.Checked)
+            {
+                yeuCauTrua = Convert.ToInt32(TruaTextBox.Text);
+            }
+            if (ToiCheckBox.Checked)
+            {
+                yeuCauToi = Convert.ToInt32(ToiTextBox.Text);
+            }
+            if (DonCheckBox.Checked)
+            {
+                yeuCauDon = DonCheckBox.Text;
+            }
+            if (HanhLyCheckBox.Checked)
+            {
+                yeuCauHanhLy = HanhLyCheckBox.Text;
+            }
+            if (MatXaCheckBox.Checked)
+            {
+                yeuCauMatXa = MatXaCheckBox.Text;
+            }
+
+            this.Hide();
+        }
+
+        private void SangCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if(SangCheckBox.Checked)
+            {
+                SangTextBox.Enabled = true;
+            }
+        }
+
+        private void TruaCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (TruaCheckBox.Checked)
+            {
+                TruaTextBox.Enabled = true;
+            }
+        }
+
+        private void ToiCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ToiCheckBox.Checked)
+            {
+                ToiTextBox.Enabled = true;
+            }
+        }
     }
 }

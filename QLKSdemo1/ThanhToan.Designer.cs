e6e674f4bc9f6dbe56f43718020eeaa520280a50
gtrax147 <kobitlaainua>
2018-10-21 15:08:34 +0700

    Duy: xong phan giao dien form ThanhToan

--- QLKSdemo1/ThanhToan.Designer.cs
+++ QLKSdemo1/ThanhToan.Designer.cs
@@ -28,20 +28,371 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.foodBillAmount = new MetroFramework.Controls.MetroLabel();
+            this.HDTALabel = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel12 = new MetroFramework.Controls.MetroLabel();
+            this.LoaiCardLabel = new MetroFramework.Controls.MetroLabel();
+            this.cvcComboBox = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel10 = new MetroFramework.Controls.MetroLabel();
+            this.NamComboBox = new MetroFramework.Controls.MetroComboBox();
+            this.ThangComboBox = new MetroFramework.Controls.MetroComboBox();
+            this.TienTongLabel = new MetroFramework.Controls.MetroLabel();
+            this.TongLabel = new MetroFramework.Controls.MetroLabel();
+            this.TaxTienLabel = new MetroFramework.Controls.MetroLabel();
+            this.TaxLabel = new MetroFramework.Controls.MetroLabel();
+            this.HDHTLabel = new MetroFramework.Controls.MetroLabel();
+            this.ThanhToanComboBox = new MetroFramework.Controls.MetroComboBox();
+            this.giaLabel = new MetroFramework.Controls.MetroLabel();
+            this.TienLabel = new MetroFramework.Controls.MetroLabel();
+            this.SoDTComboBox = new MetroFramework.Controls.MetroTextBox();
+            this.HDLabel = new MetroFramework.Controls.MetroLabel();
+            this.ThanhToanLabel = new MetroFramework.Controls.MetroLabel();
+            this.NutNext = new MetroFramework.Controls.MetroButton();
             this.SuspendLayout();
+            // 
+            // foodBillAmount
+            // 
+            this.foodBillAmount.AutoSize = true;
+            this.foodBillAmount.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.foodBillAmount.Location = new System.Drawing.Point(352, 63);
+            this.foodBillAmount.Name = "foodBillAmount";
+            this.foodBillAmount.Size = new System.Drawing.Size(0, 0);
+            this.foodBillAmount.TabIndex = 61;
+            this.foodBillAmount.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // HDTALabel
+            // 
+            this.HDTALabel.AutoSize = true;
+            this.HDTALabel.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.HDTALabel.Location = new System.Drawing.Point(10, 63);
+            this.HDTALabel.Name = "HDTALabel";
+            this.HDTALabel.Size = new System.Drawing.Size(110, 19);
+            this.HDTALabel.TabIndex = 60;
+            this.HDTALabel.Text = "Hóa đơn dịch vụ";
+            // 
+            // metroLabel12
+            // 
+            this.metroLabel12.AutoSize = true;
+            this.metroLabel12.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.metroLabel12.Location = new System.Drawing.Point(213, 253);
+            this.metroLabel12.Name = "metroLabel12";
+            this.metroLabel12.Size = new System.Drawing.Size(65, 19);
+            this.metroLabel12.TabIndex = 59;
+            this.metroLabel12.Text = "Loại thẻ :";
+            // 
+            // LoaiCardLabel
+            // 
+            this.LoaiCardLabel.AutoSize = true;
+            this.LoaiCardLabel.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.LoaiCardLabel.Location = new System.Drawing.Point(294, 254);
+            this.LoaiCardLabel.Name = "LoaiCardLabel";
+            this.LoaiCardLabel.Size = new System.Drawing.Size(66, 19);
+            this.LoaiCardLabel.TabIndex = 58;
+            this.LoaiCardLabel.Text = "Không rõ";
+            // 
+            // cvcComboBox
+            // 
+            this.cvcComboBox.BackColor = System.Drawing.Color.White;
+            // 
+            // 
+            // 
+            this.cvcComboBox.CustomButton.Image = null;
+            this.cvcComboBox.CustomButton.Location = new System.Drawing.Point(20, 1);
+            this.cvcComboBox.CustomButton.Name = "";
+            this.cvcComboBox.CustomButton.Size = new System.Drawing.Size(27, 27);
+            this.cvcComboBox.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.cvcComboBox.CustomButton.TabIndex = 1;
+            this.cvcComboBox.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.cvcComboBox.CustomButton.UseSelectable = true;
+            this.cvcComboBox.CustomButton.Visible = false;
+            this.cvcComboBox.FontSize = MetroFramework.MetroTextBoxSize.Medium;
+            this.cvcComboBox.Lines = new string[0];
+            this.cvcComboBox.Location = new System.Drawing.Point(159, 247);
+            this.cvcComboBox.MaxLength = 32767;
+            this.cvcComboBox.Name = "cvcComboBox";
+            this.cvcComboBox.PasswordChar = '\0';
+            this.cvcComboBox.PromptText = "CVC";
+            this.cvcComboBox.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.cvcComboBox.SelectedText = "";
+            this.cvcComboBox.SelectionLength = 0;
+            this.cvcComboBox.SelectionStart = 0;
+            this.cvcComboBox.ShortcutsEnabled = true;
+            this.cvcComboBox.Size = new System.Drawing.Size(48, 29);
+            this.cvcComboBox.Style = MetroFramework.MetroColorStyle.White;
+            this.cvcComboBox.TabIndex = 57;
+            this.cvcComboBox.UseCustomBackColor = true;
+            this.cvcComboBox.UseSelectable = true;
+            this.cvcComboBox.WaterMark = "CVC";
+            this.cvcComboBox.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.cvcComboBox.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroLabel10
+            // 
+            this.metroLabel10.AutoSize = true;
+            this.metroLabel10.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.metroLabel10.Location = new System.Drawing.Point(79, 253);
+            this.metroLabel10.Name = "metroLabel10";
+            this.metroLabel10.Size = new System.Drawing.Size(14, 19);
+            this.metroLabel10.TabIndex = 56;
+            this.metroLabel10.Text = "/";
+            // 
+            // NamComboBox
+            // 
+            this.NamComboBox.FormattingEnabled = true;
+            this.NamComboBox.ItemHeight = 23;
+            this.NamComboBox.Items.AddRange(new object[] {
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24"});
+            this.NamComboBox.Location = new System.Drawing.Point(99, 247);
+            this.NamComboBox.Name = "NamComboBox";
+            this.NamComboBox.PromptText = "YY";
+            this.NamComboBox.Size = new System.Drawing.Size(45, 29);
+            this.NamComboBox.TabIndex = 55;
+            this.NamComboBox.UseSelectable = true;
+            // 
+            // ThangComboBox
+            // 
+            this.ThangComboBox.FormattingEnabled = true;
+            this.ThangComboBox.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.ThangComboBox.ItemHeight = 23;
+            this.ThangComboBox.Items.AddRange(new object[] {
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "10",
+            "11",
+            "12"});
+            this.ThangComboBox.Location = new System.Drawing.Point(16, 246);
+            this.ThangComboBox.Name = "ThangComboBox";
+            this.ThangComboBox.PromptText = "MM";
+            this.ThangComboBox.Size = new System.Drawing.Size(59, 29);
+            this.ThangComboBox.TabIndex = 54;
+            this.ThangComboBox.UseSelectable = true;
+            // 
+            // TienTongLabel
+            // 
+            this.TienTongLabel.AutoSize = true;
+            this.TienTongLabel.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.TienTongLabel.Location = new System.Drawing.Point(330, 118);
+            this.TienTongLabel.Name = "TienTongLabel";
+            this.TienTongLabel.Size = new System.Drawing.Size(19, 19);
+            this.TienTongLabel.TabIndex = 53;
+            this.TienTongLabel.Text = "Đ";
+            this.TienTongLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // TongLabel
+            // 
+            this.TongLabel.AutoSize = true;
+            this.TongLabel.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.TongLabel.Location = new System.Drawing.Point(241, 118);
+            this.TongLabel.Name = "TongLabel";
+            this.TongLabel.Size = new System.Drawing.Size(43, 19);
+            this.TongLabel.TabIndex = 52;
+            this.TongLabel.Text = "Tổng";
+            // 
+            // TaxTienLabel
+            // 
+            this.TaxTienLabel.AutoSize = true;
+            this.TaxTienLabel.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.TaxTienLabel.Location = new System.Drawing.Point(339, 90);
+            this.TaxTienLabel.Name = "TaxTienLabel";
+            this.TaxTienLabel.Size = new System.Drawing.Size(19, 19);
+            this.TaxTienLabel.TabIndex = 51;
+            this.TaxTienLabel.Text = "Đ";
+            this.TaxTienLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // TaxLabel
+            // 
+            this.TaxLabel.AutoSize = true;
+            this.TaxLabel.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.TaxLabel.Location = new System.Drawing.Point(263, 90);
+            this.TaxLabel.Name = "TaxLabel";
+            this.TaxLabel.Size = new System.Drawing.Size(29, 19);
+            this.TaxLabel.TabIndex = 50;
+            this.TaxLabel.Text = "Tax";
+            // 
+            // HDHTLabel
+            // 
+            this.HDHTLabel.AutoSize = true;
+            this.HDHTLabel.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.HDHTLabel.Location = new System.Drawing.Point(10, 39);
+            this.HDHTLabel.Name = "HDHTLabel";
+            this.HDHTLabel.Size = new System.Drawing.Size(111, 19);
+            this.HDHTLabel.TabIndex = 46;
+            this.HDHTLabel.Text = "Hóa đơn hiện tại";
+            // 
+            // ThanhToanComboBox
+            // 
+            this.ThanhToanComboBox.FormattingEnabled = true;
+            this.ThanhToanComboBox.ItemHeight = 23;
+            this.ThanhToanComboBox.Items.AddRange(new object[] {
+            "Credit",
+            "Debit"});
+            this.ThanhToanComboBox.Location = new System.Drawing.Point(16, 205);
+            this.ThanhToanComboBox.Name = "ThanhToanComboBox";
+            this.ThanhToanComboBox.PromptText = "Phương thức #";
+            this.ThanhToanComboBox.Size = new System.Drawing.Size(128, 29);
+            this.ThanhToanComboBox.TabIndex = 49;
+            this.ThanhToanComboBox.UseSelectable = true;
+            // 
+            // giaLabel
+            // 
+            this.giaLabel.AutoSize = true;
+            this.giaLabel.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.giaLabel.Location = new System.Drawing.Point(386, 16);
+            this.giaLabel.Name = "giaLabel";
+            this.giaLabel.Size = new System.Drawing.Size(29, 19);
+            this.giaLabel.TabIndex = 45;
+            this.giaLabel.Text = "Giá";
+            // 
+            // TienLabel
+            // 
+            this.TienLabel.AutoSize = true;
+            this.TienLabel.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.TienLabel.Location = new System.Drawing.Point(352, 39);
+            this.TienLabel.Name = "TienLabel";
+            this.TienLabel.Size = new System.Drawing.Size(19, 19);
+            this.TienLabel.TabIndex = 44;
+            this.TienLabel.Text = "Đ";
+            this.TienLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // SoDTComboBox
+            // 
+            this.SoDTComboBox.BackColor = System.Drawing.Color.White;
+            // 
+            // 
+            // 
+            this.SoDTComboBox.CustomButton.Image = null;
+            this.SoDTComboBox.CustomButton.Location = new System.Drawing.Point(239, 1);
+            this.SoDTComboBox.CustomButton.Name = "";
+            this.SoDTComboBox.CustomButton.Size = new System.Drawing.Size(27, 27);
+            this.SoDTComboBox.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.SoDTComboBox.CustomButton.TabIndex = 1;
+            this.SoDTComboBox.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.SoDTComboBox.CustomButton.UseSelectable = true;
+            this.SoDTComboBox.CustomButton.Visible = false;
+            this.SoDTComboBox.FontSize = MetroFramework.MetroTextBoxSize.Medium;
+            this.SoDTComboBox.Lines = new string[0];
+            this.SoDTComboBox.Location = new System.Drawing.Point(159, 205);
+            this.SoDTComboBox.MaxLength = 32767;
+            this.SoDTComboBox.Name = "SoDTComboBox";
+            this.SoDTComboBox.PasswordChar = '\0';
+            this.SoDTComboBox.PromptText = "9999 - 9999 - 9999 - 9999";
+            this.SoDTComboBox.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.SoDTComboBox.SelectedText = "";
+            this.SoDTComboBox.SelectionLength = 0;
+            this.SoDTComboBox.SelectionStart = 0;
+            this.SoDTComboBox.ShortcutsEnabled = true;
+            this.SoDTComboBox.Size = new System.Drawing.Size(267, 29);
+            this.SoDTComboBox.Style = MetroFramework.MetroColorStyle.White;
+            this.SoDTComboBox.TabIndex = 48;
+            this.SoDTComboBox.UseCustomBackColor = true;
+            this.SoDTComboBox.UseSelectable = true;
+            this.SoDTComboBox.WaterMark = "9999 - 9999 - 9999 - 9999";
+            this.SoDTComboBox.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.SoDTComboBox.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // HDLabel
+            // 
+            this.HDLabel.AutoSize = true;
+            this.HDLabel.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.HDLabel.Location = new System.Drawing.Point(177, 16);
+            this.HDLabel.Name = "HDLabel";
+            this.HDLabel.Size = new System.Drawing.Size(64, 19);
+            this.HDLabel.TabIndex = 43;
+            this.HDLabel.Text = "Bảng HĐ";
+            // 
+            // ThanhToanLabel
+            // 
+            this.ThanhToanLabel.AutoSize = true;
+            this.ThanhToanLabel.BackColor = System.Drawing.Color.Transparent;
+            this.ThanhToanLabel.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.ThanhToanLabel.Location = new System.Drawing.Point(12, 183);
+            this.ThanhToanLabel.Name = "ThanhToanLabel";
+            this.ThanhToanLabel.Size = new System.Drawing.Size(79, 19);
+            this.ThanhToanLabel.TabIndex = 47;
+            this.ThanhToanLabel.Text = "Thanh toán";
+            this.ThanhToanLabel.UseCustomBackColor = true;
+            this.ThanhToanLabel.UseCustomForeColor = true;
+            // 
+            // NutNext
+            // 
+            this.NutNext.Location = new System.Drawing.Point(338, 289);
+            this.NutNext.Name = "NutNext";
+            this.NutNext.Size = new System.Drawing.Size(88, 34);
+            this.NutNext.TabIndex = 42;
+            this.NutNext.Text = "Tiếp tục";
+            this.NutNext.UseSelectable = true;
             // 
             // ThanhToan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BorderStyle = MetroFramework.Forms.MetroFormBorderStyle.FixedSingle;
-            this.ClientSize = new System.Drawing.Size(434, 284);
+            this.ClientSize = new System.Drawing.Size(437, 333);
+            this.Controls.Add(this.foodBillAmount);
+            this.Controls.Add(this.HDTALabel);
+            this.Controls.Add(this.metroLabel12);
+            this.Controls.Add(this.LoaiCardLabel);
+            this.Controls.Add(this.cvcComboBox);
+            this.Controls.Add(this.metroLabel10);
+            this.Controls.Add(this.NamComboBox);
+            this.Controls.Add(this.ThangComboBox);
+            this.Controls.Add(this.TienTongLabel);
+            this.Controls.Add(this.TongLabel);
+            this.Controls.Add(this.TaxTienLabel);
+            this.Controls.Add(this.TaxLabel);
+            this.Controls.Add(this.HDHTLabel);
+            this.Controls.Add(this.ThanhToanComboBox);
+            this.Controls.Add(this.giaLabel);
+            this.Controls.Add(this.TienLabel);
+            this.Controls.Add(this.SoDTComboBox);
+            this.Controls.Add(this.HDLabel);
+            this.Controls.Add(this.ThanhToanLabel);
+            this.Controls.Add(this.NutNext);
             this.Name = "ThanhToan";
             this.Style = MetroFramework.MetroColorStyle.Red;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroLabel foodBillAmount;
+        private MetroFramework.Controls.MetroLabel HDTALabel;
+        private MetroFramework.Controls.MetroLabel metroLabel12;
+        private MetroFramework.Controls.MetroLabel LoaiCardLabel;
+        public MetroFramework.Controls.MetroTextBox cvcComboBox;
+        private MetroFramework.Controls.MetroLabel metroLabel10;
+        public MetroFramework.Controls.MetroComboBox NamComboBox;
+        public MetroFramework.Controls.MetroComboBox ThangComboBox;
+        private MetroFramework.Controls.MetroLabel TienTongLabel;
+        private MetroFramework.Controls.MetroLabel TongLabel;
+        private MetroFramework.Controls.MetroLabel TaxTienLabel;
+        private MetroFramework.Controls.MetroLabel TaxLabel;
+        private MetroFramework.Controls.MetroLabel HDHTLabel;
+        public MetroFramework.Controls.MetroComboBox ThanhToanComboBox;
+        private MetroFramework.Controls.MetroLabel giaLabel;
+        private MetroFramework.Controls.MetroLabel TienLabel;
+        public MetroFramework.Controls.MetroTextBox SoDTComboBox;
+        private MetroFramework.Controls.MetroLabel HDLabel;
+        private MetroFramework.Controls.MetroLabel ThanhToanLabel;
+        private MetroFramework.Controls.MetroButton NutNext;
     }
 }
